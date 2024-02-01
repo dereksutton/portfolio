d6218c5df339defccc506ce41855ce94d285891f
Derek Sutton <dereksutton86@gmail.com>
2024-02-01 13:15:53 -0500

    set basic structure for homepage and added styles to components

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,37 +2,38 @@ import React from 'react';
 import heroImage from '../assets/head-split.png';
 
 const Hero = () => {
+
     return (
       <div className="bg-white text-black min-h-screen flex flex-col md:flex-row items-center justify-center px-4">
         <div className="hidden md:flex md:flex-1 justify-center">
-          <div className="w-full md:w-auto md:max-w-md"> {/* This sets a max-width for the text container */}
+          <div className="w-full md:w-auto md:max-w-md">
             <h1 className="text-4xl font-bold text-center md:text-center">designer</h1>
-            <p className="text-lg text-center md:text-center py-4">UI/UX Designer with a passion for designing elegant user experiences.</p>
+            <p className="text-lg text-center md:text-center py-4">Visual Designer & Branding Expert, dedicated to crafting compelling brand identities and visual narratives.</p>
           </div>
         </div>
 
-      <img src={heroImage} alt="Headshot" className="w-full md:w-1/3 max-w-xs md:max-w-md lg:max-w-lg mb-8 md:mb-0 rounded-[25%]" />
+      <img src={heroImage} alt="Headshot" className="w-full md:w-1/2 max-w-sm md:max-w-lg lg:max-w-xl mt-0 mb-0 md:mb-0 rounded-[25%]" />
 
       <div className="hidden md:flex md:flex-1 justify-center">
-        <div className="w-full md:w-auto md:max-w-md"> {/* This sets a max-width for the text container */}
-          <h1 className="text-4xl font-bold text-center md:text-center">{"<developer/>"}</h1>
-          <p className="text-lg text-center md:text-center py-4">Full-Stack Developer focused on writing clean, efficient code.</p>
+        <div className="w-full md:w-auto md:max-w-md">
+          <h1 className="text-4xl font-bold text-center md:text-center">{"<developer>"}</h1>
+          <p className="text-lg text-center md:text-center py-4">Full-Stack Developer focused on clean, efficient code for high-performance software solutions.</p>
         </div>
       </div>
   
         {/* Mobile layout text, visible only on mobile */}
         <div className="md:hidden flex justify-between items-center w-full px-4">
           <div className="text-center">
-            <h1 className="text-4xl font-bold">designer</h1>
-            <p className="text-lg">UI/UX Designer with a passion for designing elegant user experiences.</p>
+            <h1 className="text-4xl pb-3 font-bold">designer</h1>
+            <p className="text-md">Visual Designer & Branding Expert, dedicated to crafting compelling brand identities and visual narratives.</p>
           </div>
           
           {/* This is the vertical line divider */}
           <div className="border-l-2 border-gray-800 h-32 self-center mx-2"></div>
   
           <div className="text-center">
-            <h1 className="text-4xl font-bold">developer</h1>
-            <p className="text-lg">Full-Stack Developer focused on writing clean, efficient code.</p>
+            <h1 className="text-4xl pl-2 pb-3 font-bold">{"<developer>"}</h1>
+            <p className="text-md">Full-Stack Developer focused on clean, efficient code for high-performance software solutions.</p>
           </div>
         </div>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,68 @@
-import React from 'react';
-import logo from '../assets/DS.png';
+import React, { useState, useEffect } from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import logo from '../assets/DerekSutton.png';
+import '../App.css';
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 768);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    const handleStateChange = (state) => {
+        setMenuOpen(state.isOpen);
+    };
+
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
+    const renderMobileMenu = () => {
+        return (
+            <Menu right isOpen={menuOpen} onStateChange={handleStateChange}>
+                <a onClick={closeMenu} className="menu-item" href="#home">Home</a>
+                <a onClick={closeMenu} className="menu-item" href="#about">About</a>
+                <a onClick={closeMenu} className="menu-item" href="#portfolio">Portfolio</a>
+                <a onClick={closeMenu} className="menu-item" href="#skills">Skills</a>
+                <a onClick={closeMenu} className="menu-item" href="#contact">Contact</a>
+                <a onClick={closeMenu} href="/path/to/your/cv.pdf" target="_blank" rel="noopener noreferrer" className="menu-item mx-6 px-4 py-2 brand-coral text-white font-bold text-lg hover:bg-gray-700 rounded-full">Download CV</a>
+            </Menu>
+        );
+    };
+
+    const renderDesktopMenu = () => {
+        return (
+            <>
+                <div className="flex gap-8">
+                    <a href="#home" className="font-bold text-lg hover:text-customCoral">Home</a>
+                    <a href="#about" className="font-bold text-lg hover:text-customCoral">About</a>
+                    <a href="#portfolio" className="font-bold text-lg hover:text-customCoral">Portfolio</a>
+                    <a href="#skills" className="font-bold text-lg hover:text-customCoral">Skills</a>
+                    <a href="#contact" className="font-bold text-lg hover:text-customCoral">Contact</a>
+                </div>
+                <div>
+                    <a href="/path/to/your/cv.pdf" target="_blank" rel="noopener noreferrer" className="mx-6 px-4 py-2 brand-coral text-white font-bold text-lg hover:bg-gray-700 rounded-full">Download CV</a>
+                </div>
+            </>
+        );
+    };
+
     return (
-        <nav className="flex items-center justify-between px-4 py-0 bg-black text-white">
+        <nav className="flex items-center justify-between px-8 py-0 bg-black text-white">
             <div className="logo">
-                <img src={logo} alt="Hero" className="h-28 w-28" />
+                <img src={logo} alt="Hero" className="h-36" />
             </div>
-            <div className="flex gap-8">
-                <a href="#home" className="font-bold text-lg hover:text-gray-400">Home</a>
-                <a href="#about" className="font-bold text-lg hover:text-gray-400">About</a>
-                <a href="#portfolio" className="font-bold text-lg hover:text-gray-400">Portfolio</a>
-                <a href="#skills" className="font-bold text-lg hover:text-gray-400">Skills</a>
-                <a href="#contact" className="font-bold text-lg hover:text-gray-400">Contact</a>
-            </div>
-            <div>
-                {/* Social icons */}
-            </div>
+            {isMobile ? renderMobileMenu() : renderDesktopMenu()}
         </nav>
     );
 };
