c117f1c990e6d0155b512633832fc3485ec81131
Derek Sutton <dereksutton86@gmail.com>
2024-02-04 13:37:51 -0500

    new css edits, working on mobile responsiveness

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,7 +12,7 @@ const Hero = () => {
           </div>
         </div>
 
-      <img src={heroImage} alt="Headshot" className="w-full lg:w-1/2 max-w-md lg:max-w-lg xl:max-w-xl mt-0 mb-0 rounded-[25%]" />
+      <img src={heroImage} alt="Headshot" className="w-full lg:size-fit max-w-lg lg:max-w-lg xl:max-w-xl mt-0 mb-0 rounded-[25%]" />
 
       <div className="hidden lg:flex lg:flex-1 justify-center">
         <div className="w-full md:w-auto md:max-w-md">
@@ -24,15 +24,15 @@ const Hero = () => {
         {/* Mobile layout text, visible only on mobile */}
         <div className="lg:hidden hero-text-container w-full">
           <div className="hero-text-section">
-            <h1 className="text-4xl pb-3 font-bold">designer</h1>
-            <p className="text-md">Visual Designer & Branding Expert, dedicated to crafting compelling brand identities and visual narratives.</p>
+            <h1 className="text-3xl pb-3 font-bold">designer</h1>
+            <p className="text-sm">Visual Designer & Branding Expert, dedicated to crafting compelling brand identities and visual narratives.</p>
           </div>
           {/* This is the vertical line divider */}
           <div className="vertical-divider"></div>
   
           <div className="hero-text-section">
-            <h1 className="text-4xl pb-3 font-bold">{"<developer>"}</h1>
-            <p className="text-md">Full-Stack Developer focused on clean, efficient code for high-performance software solutions.</p>
+            <h1 className="text-3xl pb-3 font-bold">{"<developer>"}</h1>
+            <p className="text-sm">Full-Stack Developer focused on clean, efficient code for high-performance, innovative software solutions.</p>
           </div>
         </div>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -57,16 +57,16 @@ const Navbar = () => {
                     <a href="#contact" className="font-bold text-lg hover:text-customCoral">Contact</a>
                 </div>
                 <div>
-                    <a href="/path/to/your/cv.pdf" target="_blank" rel="noopener noreferrer" className="download-cv-button mx-6 px-4 py-2 brand-coral text-white font-bold text-lg hover:bg-gray-400 rounded-full">Download CV</a>
+                    <a href="/path/to/your/cv.pdf" target="_blank" rel="noopener noreferrer" className="download-cv-button mr-8 px-4 py-2 brand-coral text-white font-bold text-lg hover:bg-gray-400 rounded-full">Download CV</a>
                 </div>
             </>
         );
     };
 
     return (
-        <nav className="flex items-center justify-between px-6 py-0 bg-black text-white">
+        <nav className="flex items-center justify-between bg-black text-white">
             <div className="logo">
-                <img src={logo} alt="Hero" className="navbar-logo h-36" />
+                <img src={logo} alt="Hero" className="navbar-logo h-28 ml-8" />
             </div>
             {isMobile ? renderMobileMenu() : renderDesktopMenu()}
         </nav>
