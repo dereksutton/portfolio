707af917fbec97a540f0b7dc827d4bd582a7b0ec
Derek Sutton <dereksutton86@gmail.com>
2024-02-26 19:18:01 -0500

    removed Calendly pop-up widget, replacing with inline widget in Contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <div id="calendly-inline-widget"></div>
+        </div>
+    );
+};
+
+export default Contact;
