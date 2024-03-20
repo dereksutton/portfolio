358fa499be37c7bfd1cc0b5861b43f1c2029761a
Derek Sutton <dereksutton86@gmail.com>
2024-03-20 19:43:06 -0400

    style edit on Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -67,7 +67,7 @@ const Home = () => {
         >
           <div className="flex flex-col items-center w-full md:w-auto md:max-w-md mr-8">
             <img src={designerImg} className="w-80 h-auto"></img>
-            <p className="text-lg text-center md:text-center py-4">Visual Designer & Branding Expert, dedicated to crafting compelling brand identities and visual narratives.</p>
+            <p className="text-xl text-center md:text-center py-4">Visual Designer & Branding Expert, dedicated to crafting compelling brand identities and visual narratives.</p>
           </div>
         </motion.div>
 
